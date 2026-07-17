3cea312e3b645c147dbe98be67cee758a26416f8
Charles Nkonoki <charlitokenn@gmail.com>
2026-07-17 16:14:05 +0300

    updated site content homepage

---
+++ src/constants/index.ts
@@ -0,0 +1,20 @@
+export const appConfig = {
+    name: "Ilomo Attorneys",
+    description: "Leading the Way in Legal Solutions and Client Success",
+    url: "https://ilomoattorneys.com",
+    mobile: "+255 676 107 653",
+    email: "iloattorneys@gmail.com",
+    cta: "Your Premier Legal Team, Give us a Call",
+    images : {
+        logo: "https://ilomoattorneys.com/og-image.jpg",
+        team: {
+            partnerOne: "/team/happy-ilomo.jpg",
+            partnerTwo: "/team/joyce-basanda.jpg",
+        }
+    },
+    links: {
+        whatsapp: "wa.link",
+        facebook: "https://www.facebook.com/profile.php?id=100095239643915",
+        instagram: "https://www.instagram.com/ilomo_attoneys",
+    },
+}
